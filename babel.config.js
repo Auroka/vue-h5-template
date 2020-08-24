7e86f2baecb429bdc3a2ab73eb24afb675033c45
Auroka <auroka@163.com>
2020-08-24 14:27:09 +0800

    去除正式环境的console.log

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,19 @@
+let plugins = [
+  [
+    'import',
+    {
+      libraryName: 'vant',
+      libraryDirectory: 'es',
+      style: true
+    },
+    'vant'
+  ]
+]
+// 正式环境去除 console.log
+if (process.env.NODE_ENV === 'production') {
+  plugins.push('transform-remove-console')
+}
 module.exports = {
   presets: ['@vue/cli-plugin-babel/preset'],
-  plugins: [
-    [
-      'import',
-      {
-        libraryName: 'vant',
-        libraryDirectory: 'es',
-        style: true
-      },
-      'vant'
-    ]
-  ]
+  plugins
 }
