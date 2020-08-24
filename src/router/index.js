763d8f5e9607b0bce0363d0e8ffe7d67e794b6d0
Auroka <auroka@163.com>
2020-08-24 13:44:06 +0800

    路由跳转后滚动到顶部

--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,27 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
 
-Vue.use(VueRouter);
+Vue.use(VueRouter)
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
+    path: '/',
+    name: 'Home',
     component: Home
   },
   {
-    path: "/about",
-    name: "About",
-    component: () => import("../views/About.vue") // 路由懒加载
+    path: '/about',
+    name: 'About',
+    component: () => import('../views/About.vue') // 路由懒加载
   }
-];
+]
 
 const router = new VueRouter({
-  mode: "history",
+  mode: 'history',
   base: process.env.BASE_URL,
+  scrollBehavior: () => ({ y: 0 }),
   routes
-});
+})
 
-export default router;
+export default router
